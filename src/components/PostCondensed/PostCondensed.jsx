fe3b6bd29bd3481c9a3c4e8d902fc49334f1ee49
Vanessa Tang <vanesst5@uci.edu>
2024-04-13 22:41:37 -0700

    Added link to post detail page

--- src/components/PostCondensed/PostCondensed.jsx
+++ src/components/PostCondensed/PostCondensed.jsx
@@ -1,11 +1,14 @@
 import './PostCondensed.css'
+import { Link } from 'react-router-dom'
 
 const PostCondensed = ({post}) => {
   return (
     <div className='post'>
-      <h5 className='posted-time'>Posted on {post.created_at}</h5>
-      <h4 className='post-title'>{post.title}</h4>
-      <p className='upvotes'>Upvotes: {post.upvotes}</p>
+      <Link className='post-link' to={`/post/${post.id}`}>
+        <h5 className='posted-time'>Posted on {post.created_at}</h5>
+        <h4 className='post-title'>{post.title}</h4>
+        <p className='upvotes'>Upvotes: {post.upvotes}</p>
+      </Link>
     </div>
   )
 }
