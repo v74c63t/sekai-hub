3edffd8bc6cffee08747f0e1cc78c77cee750f1c
Vanessa Tang <vanesst5@uci.edu>
2024-04-14 17:43:07 -0700

    Added flairs to posts

--- src/components/PostCondensed/PostCondensed.jsx
+++ src/components/PostCondensed/PostCondensed.jsx
@@ -1,12 +1,13 @@
 import './PostCondensed.css'
 import { Link } from 'react-router-dom'
 
-const PostCondensed = ({post}) => {
+const PostCondensed = ({post, uid}) => {
+
   return (
     <div className='post'>
       <Link className='post-link' to={`/post/${post.id}`}>
-        <h5 className='posted-time'>Posted on {post.created_at}</h5>
-        <h4 className='post-title'>{post.title}</h4>
+        <h5 className='posted-time'>Posted by @{post.uid} on {post.created_at}</h5>
+        <h4 className='post-title'>{post.title} <span className={`flair ${post.flair}`}>{post.flair}</span></h4>
         <p className='upvotes'>Upvotes: {post.upvotes}</p>
       </Link>
     </div>
