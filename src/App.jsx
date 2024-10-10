827e9096220bb6b56cfee5a5ae646c4577b5b81f
Vanessa Tang <vanesst5@uci.edu>
2024-10-10 02:27:33 -0700

    Fixed responsiveness for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,240 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Link } from 'react-router-dom'
-import PostCondensed from './components/PostCondensed/PostCondensed'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import PostCondensed from "./components/PostCondensed/PostCondensed";
 import { useOutletContext, useNavigate } from "react-router-dom";
-import { supabase } from './config/SupabaseClient';
-import AddIcon from '@mui/icons-material/Add';
-import { ring2 } from 'ldrs'
+import { supabase } from "./config/SupabaseClient";
+import AddIcon from "@mui/icons-material/Add";
+import { ring2 } from "ldrs";
 
 function App() {
+	const [UID, theme] = useOutletContext();
+	const navigate = useNavigate();
 
-  const [UID, theme] = useOutletContext()
-  const navigate = useNavigate()
+	const [posts, setPosts] = useState([]);
 
-  const [posts, setPosts] = useState([])
+	const [sortByFilter, setSortByFilter] = useState("newest");
 
-  const [sortByFilter, setSortByFilter] = useState('newest')
+	const [filterByFilter, setFilterByFilter] = useState(null);
 
-  const [filterByFilter, setFilterByFilter] = useState(null)
+	const [loading, setLoading] = useState(true);
 
-  const [loading, setLoading] = useState(true)
+	ring2.register();
 
-  ring2.register()
+	useEffect(() => {
+		setLoading(true);
+		const fetchPosts = async () => {
+			const { data } = await supabase
+				.from("posts")
+				.select("id, created_at, title, upvotes, user_id, flair")
+				.order("created_at", { ascending: false });
+			setPosts(data);
+			setLoading(false);
+		};
+		fetchPosts();
+	}, []);
 
-  useEffect(() => {
-    setLoading(true)
-    const fetchPosts = async() => {
-      const {data} = await supabase
-                          .from('posts')
-                          .select('id, created_at, title, upvotes, user_id, flair')
-                          .order('created_at', {ascending: false})
-      setPosts(data)
-      setLoading(false)
-    }
-    fetchPosts()
-  }, [])
+	const handleSort = async (event) => {
+		if (sortByFilter !== event.target.id) {
+			setLoading(true);
+			setSortByFilter(event.target.id);
+			if (event.target.id === "newest") {
+				if (filterByFilter !== null) {
+					const { data } = await supabase
+						.from("posts")
+						.select(
+							"id, created_at, title, upvotes, user_id, flair"
+						)
+						.eq(
+							"flair",
+							filterByFilter.charAt(0).toUpperCase() +
+								filterByFilter.slice(1)
+						)
+						.order("created_at", { ascending: false });
+					setPosts(data);
+				} else {
+					const { data } = await supabase
+						.from("posts")
+						.select(
+							"id, created_at, title, upvotes, user_id, flair"
+						)
+						.order("created_at", { ascending: false });
+					setPosts(data);
+				}
+			} else if (event.target.id === "most-popular") {
+				if (filterByFilter !== null) {
+					const { data } = await supabase
+						.from("posts")
+						.select(
+							"id, created_at, title, upvotes, user_id, flair"
+						)
+						.eq(
+							"flair",
+							filterByFilter.charAt(0).toUpperCase() +
+								filterByFilter.slice(1)
+						)
+						.order("upvotes", { ascending: false });
+					setPosts(data);
+				} else {
+					const { data } = await supabase
+						.from("posts")
+						.select(
+							"id, created_at, title, upvotes, user_id, flair"
+						)
+						.order("upvotes", { ascending: false });
+					setPosts(data);
+				}
+			}
+			setLoading(false);
+		}
+	};
 
-  const handleSort = async (event) => {
-    if(sortByFilter !== event.target.id) {
-      setLoading(true)
-      setSortByFilter(event.target.id)
-      if(event.target.id === 'newest') {
-        if(filterByFilter !== null) {
-          const {data} = await supabase
-                              .from('posts')
-                              .select('id, created_at, title, upvotes, user_id, flair')
-                              .eq('flair', (filterByFilter.charAt(0).toUpperCase() + filterByFilter.slice(1)))
-                              .order('created_at', {ascending: false})
-          setPosts(data)
-        }
-        else {
-          const {data} = await supabase
-                              .from('posts')
-                              .select('id, created_at, title, upvotes, user_id, flair')
-                              .order('created_at', {ascending: false})
-          setPosts(data)
-        }
-      }
-      else if(event.target.id === 'most-popular') {
-        if(filterByFilter !== null) {
-          const {data} = await supabase
-                              .from('posts')
-                              .select('id, created_at, title, upvotes, user_id, flair')
-                              .eq('flair', (filterByFilter.charAt(0).toUpperCase() + filterByFilter.slice(1)))
-                              .order('upvotes', {ascending: false})
-          setPosts(data)
-        }
-        else {
-          const {data} = await supabase
-                              .from('posts')
-                              .select('id, created_at, title, upvotes, user_id, flair')
-                              .order('upvotes', {ascending: false})
-          setPosts(data)
-        }
-      }
-      setLoading(false)
-    }
-  }
+	const handleFilter = async (event) => {
+		setLoading(true);
+		const order = sortByFilter === "newest" ? "created_at" : "upvotes";
+		if (filterByFilter !== event.target.id) {
+			setFilterByFilter(event.target.id);
+			const { data } = await supabase
+				.from("posts")
+				.select("id, created_at, title, upvotes, user_id, flair")
+				.eq(
+					"flair",
+					event.target.id.charAt(0).toUpperCase() +
+						event.target.id.slice(1)
+				)
+				.order(order, { ascending: false });
+			setPosts(data);
+		} else {
+			setFilterByFilter(null);
+			const { data } = await supabase
+				.from("posts")
+				.select("id, created_at, title, upvotes, user_id, flair")
+				.order(order, { ascending: false });
+			setPosts(data);
+		}
+		setLoading(false);
+	};
 
-  const handleFilter = async(event) => {
-    setLoading(true)
-    const order = sortByFilter === 'newest' ? 'created_at' : 'upvotes'
-    if(filterByFilter !== event.target.id) {
-      setFilterByFilter(event.target.id)
-      const {data} = await supabase
-                            .from('posts')
-                            .select('id, created_at, title, upvotes, user_id, flair')
-                            .eq('flair', (event.target.id.charAt(0).toUpperCase() + event.target.id.slice(1)))
-                            .order(order, {ascending: false})
-      setPosts(data)
-    }
-    else {
-      setFilterByFilter(null)
-      const {data} = await supabase
-                            .from('posts')
-                            .select('id, created_at, title, upvotes, user_id, flair')
-                            .order(order, {ascending: false})
-      setPosts(data)
-    }
-    setLoading(false)
-  }
-
-  return (
-    <>
-      <div className='sort-filter'>
-        <div className='sort-container'>
-          <h3 className='sort'>Sort By:</h3>
-          <h4 id='newest' className={sortByFilter === 'newest' ? `sort-flair ${theme}-bg active` : `sort-flair ${theme}-bg ${theme}-bg-hover`} onClick={handleSort}>Newest</h4>
-          <h4 id='most-popular' className={sortByFilter === 'most-popular' ? `sort-flair ${theme}-bg active` : `sort-flair ${theme}-bg ${theme}-bg-hover`} onClick={handleSort}>Most Popular</h4>
-        </div>
-        <div className='filter-container'>
-          <h3 className='filter'>Filter By:</h3>
-          <h4 id='discussion' className={filterByFilter !== null && filterByFilter === 'discussion' ? `filter-flair ${theme}-bg discussion active` : `filter-flair ${theme}-bg discussion ${theme}-bg-hover`} onClick={handleFilter}>Discussion</h4>
-          <h4 id='achievements' className={filterByFilter !== null && filterByFilter === 'achievements' ? `filter-flair ${theme}-bg achievements active` : `filter-flair ${theme}-bg achievements ${theme}-bg-hover`} onClick={handleFilter}>Achievements</h4>
-          <h4 id='question' className={filterByFilter !== null && filterByFilter === 'question' ? `filter-flair ${theme}-bg question active` : `filter-flair ${theme}-bg question ${theme}-bg-hover`} onClick={handleFilter}>Question</h4>
-          <h4 id='gameplay' className={filterByFilter !== null && filterByFilter === 'gameplay' ? `filter-flair ${theme}-bg gameplay active` : `filter-flair ${theme}-bg gameplay ${theme}-bg-hover`} onClick={handleFilter}>Gameplay</h4>
-        </div>
-      </div>
-      {
-        loading ? (
-          <div className='loading'>
-            <h1>Loading</h1>
-            <l-ring-2
-              size="40"
-              speed="0.8"
-              bg-opacity={0.3}
-              stroke={5}
-              stroke-length={0.25}
-              color={'gray'}
-            ></l-ring-2>
-          </div>
-        )
-        :
-        posts.length !== 0 ? (
-          <div className='posts-container'>
-            {posts.map((post, i) => {
-              return (
-                <PostCondensed key={i} post={post} />
-              )
-            })}
-          </div>
-        ) :
-        <div className='no-posts'>
-          <p>There are no posts currently.</p>
-          <p>Get started on contributing by <Link className={`${theme} create-link`} to="/create">creating a new post</Link>!</p>
-        </div>
-      }
-      <div className='fab-container'>
-        <button className={`${theme}-bg fab`} onClick={()=>navigate('/create')}>
-          <AddIcon sx={{ fontSize: 30 }} className='fab-icon' />
-        </button>
-      </div>
-    </>
-  )
+	return (
+		<div>
+			<div className="sort-filter">
+				<div className="sort-container">
+					<h3 className="sort">Sort By:</h3>
+					<h4
+						id="newest"
+						className={
+							sortByFilter === "newest"
+								? `sort-flair ${theme}-bg active`
+								: `sort-flair ${theme}-bg ${theme}-bg-hover`
+						}
+						onClick={handleSort}
+					>
+						Newest
+					</h4>
+					<h4
+						id="most-popular"
+						className={
+							sortByFilter === "most-popular"
+								? `sort-flair ${theme}-bg active`
+								: `sort-flair ${theme}-bg ${theme}-bg-hover`
+						}
+						onClick={handleSort}
+					>
+						Most Popular
+					</h4>
+				</div>
+				<div className="filter-container">
+					<h3 className="filter">Filter By:</h3>
+					<h4
+						id="discussion"
+						className={
+							filterByFilter !== null &&
+							filterByFilter === "discussion"
+								? `filter-flair ${theme}-bg discussion active`
+								: `filter-flair ${theme}-bg discussion ${theme}-bg-hover`
+						}
+						onClick={handleFilter}
+					>
+						Discussion
+					</h4>
+					<h4
+						id="achievements"
+						className={
+							filterByFilter !== null &&
+							filterByFilter === "achievements"
+								? `filter-flair ${theme}-bg achievements active`
+								: `filter-flair ${theme}-bg achievements ${theme}-bg-hover`
+						}
+						onClick={handleFilter}
+					>
+						Achievements
+					</h4>
+					<h4
+						id="question"
+						className={
+							filterByFilter !== null &&
+							filterByFilter === "question"
+								? `filter-flair ${theme}-bg question active`
+								: `filter-flair ${theme}-bg question ${theme}-bg-hover`
+						}
+						onClick={handleFilter}
+					>
+						Question
+					</h4>
+					<h4
+						id="gameplay"
+						className={
+							filterByFilter !== null &&
+							filterByFilter === "gameplay"
+								? `filter-flair ${theme}-bg gameplay active`
+								: `filter-flair ${theme}-bg gameplay ${theme}-bg-hover`
+						}
+						onClick={handleFilter}
+					>
+						Gameplay
+					</h4>
+				</div>
+			</div>
+			{loading ? (
+				<div className="loading">
+					<h1>Loading</h1>
+					<l-ring-2
+						size="40"
+						speed="0.8"
+						bg-opacity={0.3}
+						stroke={5}
+						stroke-length={0.25}
+						color={"gray"}
+					></l-ring-2>
+				</div>
+			) : posts.length !== 0 ? (
+				<div className="posts-container">
+					{posts.map((post, i) => {
+						return <PostCondensed key={i} post={post} />;
+					})}
+				</div>
+			) : (
+				<div className="no-posts">
+					<p>There are no posts currently.</p>
+					<p>
+						Get started on contributing by{" "}
+						<Link className={`${theme} create-link`} to="/create">
+							creating a new post
+						</Link>
+						!
+					</p>
+				</div>
+			)}
+			<div className="fab-container">
+				<button
+					className={`${theme}-bg fab`}
+					onClick={() => navigate("/create")}
+				>
+					<AddIcon sx={{ fontSize: 30 }} className="fab-icon" />
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,87 +1,291 @@
-import './Navbar.css'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import { nanoid } from 'nanoid'
-import { useState } from 'react'
-import { Icon } from '@iconify/react/dist/iconify.js'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
+import "./Navbar.css";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const NavBar = () => {
-  const [UID, setUID] = useState(nanoid(12))
+	const [UID, setUID] = useState(nanoid(12));
 
-  const [theme, setTheme] = useState('vs')
+	const [theme, setTheme] = useState("vs");
 
-  const [anchorEl, setAnchorEl] = useState(null);
+	const [anchorEl, setAnchorEl] = useState(null);
 
-  const [query, setQuery] = useState('')
+	const [query, setQuery] = useState("");
 
-  const navigate = useNavigate()
+	const navigate = useNavigate();
 
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleSearch = (event) => {
-    if(event.key === 'Enter') {
-      if(query.replace(/\s/g, '') !== '') {
-        navigate(`/search/${query}`)
-        setQuery('')
-      }
-    }
-  }
+	const handleSearch = (event) => {
+		if (event.key === "Enter") {
+			if (query.replace(/\s/g, "") !== "") {
+				navigate(`/search/${query}`);
+				setQuery("");
+			}
+		}
+	};
 
-  return (
-    <div>
-      <div className="nav-bar-container">
-        <nav className={`nav-bar ${theme}-bg`}>
-          <div className="nav-bar-list">
-            <img src="/favicon_io/android-chrome-192x192.png" alt="" width={40} />
-            <Link className='header-link' to="/">
-              <h2 className='header'>Sekai Hub</h2>
-            </Link>
-            <Link className='link' to="/">
-              Home
-            </Link>
-            <Link className='link' to="/create">
-              Create New Post
-            </Link>
-          </div>
-          <input className='search' type="text" placeholder="Search..." value={query} onChange={(event)=>setQuery(event.target.value)} onKeyDown={handleSearch} />
-          <div className="user-info">
-            <p className='navbar-uid'>@{UID}</p>
-            <Icon className='settings-icon' icon="fluent:text-grammar-settings-24-filled" onClick={handleClick} />
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              keepMounted
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-                disablePadding: true,
-              }}
-              sx={{borderRadius: '1rem', marginTop: '1.5rem'}}
-            >
-              <h3 className={`theme-settings-header ${theme}-bg`}>Theme</h3>
-              <MenuItem onClick={() => {setTheme('vs'); handleClose()}} selected={theme === 'vs'}><img src="/assets/vs-logo.jpg" alt="virtual singer logo" height={50} width={'auto'} /></MenuItem>
-              <MenuItem onClick={() => {setTheme('ln'); handleClose()}} selected={theme === 'ln'}><img src="/assets/ln-logo.jpg" alt="leo/need logo" height={50} width={'auto'} /></MenuItem>
-              <MenuItem onClick={() => {setTheme('mmj'); handleClose()}} selected={theme === 'mmj'}><img src="/assets/mmj-logo.jpg" alt="more more jump logo" height={50} width={'auto'} /></MenuItem>
-              <MenuItem onClick={() => {setTheme('vbs'); handleClose()}} selected={theme === 'vbs'}><img src="/assets/vbs-logo.jpg" alt="vivid bad squad logo" height={50} width={'auto'} /></MenuItem>
-              <MenuItem onClick={() => {setTheme('wxs'); handleClose()}} selected={theme === 'wxs'}><img src="/assets/wxs-logo-jp.jpg" alt="wonderlands x showtime logo" height={50} width={'auto'} /></MenuItem>
-              <MenuItem onClick={() => {setTheme('n25'); handleClose()}} selected={theme === 'n25'}><img src="/assets/n25-logo-jp.jpg" alt="nightcord logo" height={50} width={'auto'} /></MenuItem>
-            </Menu>
-          </div>
-        </nav>
-      </div>
-      <div className="page">
-        <Outlet context={[UID, theme]} />
-      </div>
-    </div>
-  )
-}
+	return (
+		<>
+			<div className="nav-bar-container">
+				<nav className={`nav-bar ${theme}-bg`}>
+					<div className="nav-bar-list">
+						<Link to="/">
+							<img
+								src="/favicon_io/android-chrome-192x192.png"
+								alt=""
+								width={40}
+							/>
+						</Link>
+						<Link className="header-link" to="/">
+							<h2 className="header">Sekai Hub</h2>
+						</Link>
+						<Link className="link" to="/">
+							Home
+						</Link>
+						<Link className="link" to="/create">
+							Create New Post
+						</Link>
+					</div>
+					<input
+						className="search"
+						type="text"
+						placeholder="Search..."
+						value={query}
+						onChange={(event) => setQuery(event.target.value)}
+						onKeyDown={handleSearch}
+					/>
+					<div className="user-info">
+						<p className="navbar-uid">@{UID}</p>
+						<Icon
+							className="settings-icon"
+							icon="fluent:text-grammar-settings-24-filled"
+							onClick={handleClick}
+						/>
+						<Menu
+							id="basic-menu"
+							anchorEl={anchorEl}
+							open={open}
+							keepMounted
+							onClose={handleClose}
+							MenuListProps={{
+								"aria-labelledby": "basic-button",
+								disablePadding: true,
+							}}
+							sx={{ borderRadius: "1rem", marginTop: "1.5rem" }}
+						>
+							<h3 className={`theme-settings-header ${theme}-bg`}>
+								Theme
+							</h3>
+							<MenuItem
+								onClick={() => {
+									setTheme("vs");
+									handleClose();
+								}}
+								selected={theme === "vs"}
+							>
+								<img
+									src="/assets/vs-logo.jpg"
+									alt="virtual singer logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("ln");
+									handleClose();
+								}}
+								selected={theme === "ln"}
+							>
+								<img
+									src="/assets/ln-logo.jpg"
+									alt="leo/need logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("mmj");
+									handleClose();
+								}}
+								selected={theme === "mmj"}
+							>
+								<img
+									src="/assets/mmj-logo.jpg"
+									alt="more more jump logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("vbs");
+									handleClose();
+								}}
+								selected={theme === "vbs"}
+							>
+								<img
+									src="/assets/vbs-logo.jpg"
+									alt="vivid bad squad logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("wxs");
+									handleClose();
+								}}
+								selected={theme === "wxs"}
+							>
+								<img
+									src="/assets/wxs-logo-jp.jpg"
+									alt="wonderlands x showtime logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("n25");
+									handleClose();
+								}}
+								selected={theme === "n25"}
+							>
+								<img
+									src="/assets/n25-logo-jp.jpg"
+									alt="nightcord logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+						</Menu>
+					</div>
+					<div className="mobile-user-info">
+						<Icon
+							className="settings-icon"
+							icon="fluent:text-grammar-settings-24-filled"
+							onClick={handleClick}
+						/>
+						<Menu
+							id="basic-menu"
+							anchorEl={anchorEl}
+							open={open}
+							keepMounted
+							onClose={handleClose}
+							MenuListProps={{
+								"aria-labelledby": "basic-button",
+								disablePadding: true,
+							}}
+							sx={{ borderRadius: "1rem", marginTop: "1.5rem" }}
+						>
+							<h3 className={`theme-settings-header ${theme}-bg`}>
+								Theme
+							</h3>
+							<MenuItem
+								onClick={() => {
+									setTheme("vs");
+									handleClose();
+								}}
+								selected={theme === "vs"}
+							>
+								<img
+									src="/assets/vs-logo.jpg"
+									alt="virtual singer logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("ln");
+									handleClose();
+								}}
+								selected={theme === "ln"}
+							>
+								<img
+									src="/assets/ln-logo.jpg"
+									alt="leo/need logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("mmj");
+									handleClose();
+								}}
+								selected={theme === "mmj"}
+							>
+								<img
+									src="/assets/mmj-logo.jpg"
+									alt="more more jump logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("vbs");
+									handleClose();
+								}}
+								selected={theme === "vbs"}
+							>
+								<img
+									src="/assets/vbs-logo.jpg"
+									alt="vivid bad squad logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("wxs");
+									handleClose();
+								}}
+								selected={theme === "wxs"}
+							>
+								<img
+									src="/assets/wxs-logo-jp.jpg"
+									alt="wonderlands x showtime logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									setTheme("n25");
+									handleClose();
+								}}
+								selected={theme === "n25"}
+							>
+								<img
+									src="/assets/n25-logo-jp.jpg"
+									alt="nightcord logo"
+									height={50}
+									width={"auto"}
+								/>
+							</MenuItem>
+						</Menu>
+					</div>
+				</nav>
+			</div>
+			<div className="page">
+				<Outlet context={[UID, theme]} />
+			</div>
+		</>
+	);
+};
 
-export default NavBar
+export default NavBar;
